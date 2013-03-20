a7e2f2b5527bdd00c672deec126d0421282370cf
Stephen Gross <smgross@lbl.gov>
2013-03-19 20:25:06 -0700

    added better options

--- getmd5sums.pl
+++ getmd5sums.pl
@@ -5,30 +5,27 @@ use Pod::Usage;
 
 
 =head1 NAME
-
-	getmd5sums.pl   Get md5 checksums for all files in a directory
-
+	
+	getmd5sums.pl  [OPTIONS] <FILEs> Get md5 checksums for files
+	
 =head1 SYNOPSIS
-
-	getmd5sums.pl [options]
 	
-	Returns a table of file names and md5 checksums to standard output
-	
-	Options:
-		-dir	directory to use. Default = "." (working directory)
+	Options: 
+		-dir	get md5sums for all files in directory (. for cwd)
 		-man	display full help
 		-help	display quick help (you're looking at it)
 
 =head1 AUTHOR
-
-	Stephen Gross   2013
-
+	
+	Stephen Gross  smgross@lbl.gov  2013
+	
 =cut
 
 
-my $dir = ".";
+my $dir;
 my $man;
 my $help;
+my @files;
 
 GetOptions (
 	"dir=s" => \$dir,
@@ -39,18 +36,20 @@ GetOptions (
 pod2usage() if $help;
 pod2usage(-verbose => 2) if $man;
 
+@files = @ARGV;
 
-#remove any trailing / from $dir
-
-$dir =~ s/\/$//g;
-
-my $listing = `ls -1 $dir`;
-
-my @files = split(/\n/, $listing);
+if ($dir) {
+	$dir =~ s/\/$//g;  #remove trailing slash (if any)
+	$dir = "$dir"."/";  #add it back
+	my $listing = `ls -1 $dir`;
+	@files = split(/\n/, $listing);
+	} else {
+	$dir = "./";
+	};
 
 my %results;
 for (my $i = 0; $i < @files; $i++) {
-	my $filename = "$dir"."/"."$files[$i]";
+	my $filename = "$dir"."$files[$i]";
 	my $checksum = `md5sum $filename`;
 	$checksum =~ s/$filename//g;
 	$checksum =~ s/^\s+//g;
@@ -59,6 +58,12 @@ for (my $i = 0; $i < @files; $i++) {
 	};
 
 print "FILENAME\tmd5 CHECKSUM\n";
-for my $file (keys %results) {
-	print "$file\t$results{$file}\n";
+for my $filename (keys %results) {
+	my $printfilename = $filename;
+	if ($printfilename =~ m/^\.\//) {
+		$printfilename =~ s/^\.\///;
+		};
+	print "$printfilename\t$results{$filename}\n";
 	};
+
+exit;
